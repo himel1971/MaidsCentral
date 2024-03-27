83e5f6bbddee78f01a9cf8be164e95ac3677ad33
himel1971 <hemel01@outlook.com>
2024-03-28 04:29:03 +0600

    Map& Blog page added

--- src/assets/Component/Contact/MapBox.jsx
+++ src/assets/Component/Contact/MapBox.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { Map } from 'pigeon-maps'
-import { osm } from 'pigeon-maps/providers'
-import Location from "./Location";
+import React, { useState } from "react";
+import { Map, Marker } from "pigeon-maps"
+
 import LocationList from "./LocationList";
 
 const MapBox = () => {
+  const [hue, setHue] = useState(0)
+  const color = `hsl(${hue % 360}deg 39% 70%)`
+
+
   return (
     <>
       <section className="relative z-10 overflow-hidden px-8  py-20 dark:bg-dark lg:py-[120px]">
@@ -19,10 +22,10 @@ const MapBox = () => {
                   GET IN TOUCH WITH US
                 </h2>
                 <p className="mb-9 text-base leading-relaxed text-body-color dark:text-dark-6">
-                Get in touch with Maids Central today for reliable and trustworthy domestic help services. Whether you need assistance with cleaning, childcare, or eldercare, our skilled maids are here to help. Feel free to reach out via phone, email, or WhatsApp. We look forward to assisting you with your household needs!
+                  Get in touch with Maids Central today for reliable and trustworthy domestic help services. Whether you need assistance with cleaning, childcare, or eldercare, our skilled maids are here to help. Feel free to reach out via phone, email, or WhatsApp. We look forward to assisting you with your household needs!
                 </p>
-               
-            
+
+
 
 
 
@@ -36,6 +39,123 @@ const MapBox = () => {
 
             <LocationList></LocationList>
           </div>
+
+
+          {/* map box */}
+
+
+          <div className=" mt-24 space-y-10">
+
+
+            <h1 className="text-4xl font-semibold uppercase text-[#065FB1] ">Find us On Map</h1>
+
+
+
+            {/* 1st location  */}
+
+            <div>
+              <h1 className="text-xl font-medium uppercase text-slate-700 mb-2 ">BUKIT TIMAH</h1>
+
+
+              <Map height={300} defaultCenter={[1.343070, 103.776090]} defaultZoom={16}>
+                <Marker
+                  width={50}
+                  anchor={[1.343070, 103.776090]}
+                  color={color}
+                  onClick={() => setHue(hue + 20)}
+                />
+                <Marker
+                  width={50}
+                  anchor={[1.343070, 103.776090]}
+                  color={color}
+                  onClick={() => {
+                    setHue(hue + 20);
+                    window.open("https://www.google.com/maps/dir//170+Upper+Bukit+Timah+Rd,+Singapore+588179/@1.3431425,103.6934771,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x31da11d085b7bbbf:0xa1c1d2dd91fe299d!2m2!1d103.7758791!2d1.3431439?entry=ttu", "_blank");
+                  }}
+                >
+
+
+
+
+                  {/* <img src="../../../../public/images/icons/icon-phone.svg" alt="" /> */}
+                </Marker>
+              </Map>
+            </div>
+
+            {/* 2nd location  */}
+
+            <div>
+              <h1 className="text-xl font-medium uppercase text-slate-700 mb-2 ">THOMSON</h1>
+
+
+              <Map height={300} defaultCenter={[1.351980, 103.836000]} defaultZoom={16}>
+                <Marker
+                  width={50}
+                  anchor={[1.351980, 103.836000]}
+                  color={color}
+                  onClick={() => setHue(hue + 20)}
+                />
+                <Marker
+                  width={50}
+                  anchor={[1.351980, 103.836000]}
+                  color={color}
+                  onClick={() => {
+                    setHue(hue + 20);
+                    window.open("https://www.google.com/maps/dir//200+Upper+Thomson+Road,+Singapore+574424,+Singapore/@1.351845,103.7948307,13z/data=!3m1!5s0x31da1736273e05e5:0x991c266b75ccd8f0!4m8!4m7!1m0!1m5!1m1!1s0x31da173a5329dd59:0x944f4bbcfcc3b83c!2m2!1d103.8360304!2d1.3518453?entry=ttu", "_blank");
+                  }}
+                >
+
+
+
+
+                  {/* <img src="../../../../public/images/icons/icon-phone.svg" alt="" /> */}
+                </Marker>
+              </Map>
+            </div>
+            {/* 3rd location  */}
+
+
+
+            <div>
+              <h1 className="text-xl font-medium uppercase text-slate-700 mb-2 ">GEYLANG</h1>
+
+
+              <Map height={300} defaultCenter={[1.316870, 103.899130]} defaultZoom={16}>
+                <Marker
+                  width={50}
+                  anchor={[1.316870, 103.899130]}
+                  color={color}
+                  onClick={() => setHue(hue + 20)}
+                />
+                <Marker
+                  width={50}
+                  anchor={[1.316870, 103.899130]}
+                  color={color}
+                  onClick={() => {
+                    setHue(hue + 20);
+                    window.open("https://www.google.com/maps/dir//%2301-19+Dewi+Gift+Shop,+55+Changi+Rd,+Singapore+419709/@1.3167155,103.8710616,13.5z/data=!3m1!5s0x31da1736273e05e5:0x991c266b75ccd8f0!4m8!4m7!1m0!1m5!1m1!1s0x31da18109d77b377:0x105e4875aaf1dc4b!2m2!1d103.8990002!2d1.3168188?entry=ttu", "_blank");
+                  }}
+                >
+
+
+
+
+                  {/* <img src="../../../../public/images/icons/icon-phone.svg" alt="" /> */}
+                </Marker>
+              </Map>
+            </div>
+
+
+
+
+
+
+
+
+
+
+          </div>
+
         </div>
       </section>
     </>
--- src/assets/Component/Home/Blog/BlogDetails.jsx
+++ src/assets/Component/Home/Blog/BlogDetails.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const BlogDetails = () => {
     const { id } = useParams();
     const allBlogs = useLoaderData();
     const blog = allBlogs.find(maid => maid._id == id);
-    // console.log(blog);
-    const { title, content, _id, featured_image_url } = blog;
+
+    if (!blog) {
+        return <div>Loading...</div>;
+    }
+
+    const { title, content, featured_image_url } = blog;
+
     return (
         <div className='font-rubik'>
             <div className="p-5 mx-auto sm:p-10 md:p-16 bg-gray-100 text-gray-800">
                 <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-                    <img src={featured_image_url} alt="" className="w-full bg-cover  sm:h-96 bg-gray-500" />
+                    <img src={featured_image_url} alt={title} className="w-full h-96 object-cover bg-gray-500" />
                     <div className="p-6 pb-12 container mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-50">
                         <div className="space-y-2">
-                            <h2 rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl text-[#065FB1]">
-                            {title}
+                            <h2 className="inline-block text-2xl font-semibold sm:text-3xl text-[#065FB1]">
+                                {title}
                             </h2>
-
-
-
-                            <p className="text-xs text-gray-600">By <span rel="noopener noreferrer" href="#" className="text-xs hover:underline">Maids Central</span>
-                            </p>
+                            <p className="text-xs text-gray-600">By <span className="text-xs hover:underline">Maids Central</span></p>
                         </div>
-                        <div className="text-gray-800  font-normal leading-7">
+                        <div className="text-gray-800 font-normal leading-7">
                             <div dangerouslySetInnerHTML={{ __html: content }} />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default BlogDetails
+export default BlogDetails;
